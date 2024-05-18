7de8802f8f1cf8366df6145660ebc4be069f7751
Akanimo Sam Okure <akan.okure@gmail.com>
2024-05-18 08:24:40 +0100

    fix deployment issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2030{" "}
+        © 2024{" "}
         <Link to="/" className="hover:underline">
           Movies App
         </Link>
@@ -12,22 +12,22 @@ export const Footer = () => {
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">
-            Instagram
-          </a>
-        </li>
-        <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">
+          <a
+            href="https://www.linkedin.com/in/a-sam-okure-09b22516/"
+            target="_blank"
+            rel="noreferrer"
+            className="mr-4 hover:underline md:mr-6"
+          >
             LinkedIn
           </a>
         </li>
+
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">
-            Youtube
-          </a>
-        </li>
-        <li>
-          <a href="#" target="_blank" className="hover:underline">
+          <a
+            href="https://github.com/ASOkure"
+            target="_blank"
+            className="hover:underline"
+          >
             Github
           </a>
         </li>
